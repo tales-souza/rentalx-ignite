32dac7b0cb126ae0d6cbb9dfe759a358fffc41c3
Tales <tales.monteiro@hotmail.com>
2022-03-04 17:51:24 -0300

    Criando o CreateRentalUseCase (Em desenvolvimento)

--- src/shared/infra/typeorm/migrations/1643888123870-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1643888123870-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1643888123870 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
