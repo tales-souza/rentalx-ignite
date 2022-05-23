4784ef64002aaab0c41158c9205eea2e09133f32
Tales <tales.monteiro02@hotmail.com>
2022-05-23 09:38:57 -0300

    correcao

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,10 +1,7 @@
-declare global {
-  declare namespace Express {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    export interface Request {
-      user: {
-        id: string;
-      };
-    }
+declare namespace Express {
+  export interface Request {
+    user: {
+      id: string;
+    };
   }
 }
