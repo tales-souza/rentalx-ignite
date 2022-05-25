fdc59544d9d2644d503e90d2cec3b2616e2cfe6c
Tales <tales.monteiro02@hotmail.com>
2022-05-25 20:23:04 -0300

    Ajuste index.d.ts 3

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -2,10 +2,8 @@ declare namespace Express {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   export interface Request {
     user: {
-      // eslint-disable-next-line prettier/prettier
-      id: string
-      // eslint-disable-next-line prettier/prettier
-    }
+      id: string;
+    };
   }
   // eslint-disable-next-line prettier/prettier
 };
