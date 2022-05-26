2821de5331130a1f29e8b73692966af54880d20f
Tales <tales.monteiro02@hotmail.com>
2022-05-25 22:47:28 -0300

    correção final Babel

--- Babel.config.js
+++ Babel.config.js
@@ -1,27 +1,23 @@
-
-
 module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript"
+    "@babel/preset-typescript",
   ],
   plugins: [
     [
       "module-resolver",
       {
         alias: {
-          "@modules": ["./src/modules"],
-          "@config": ["./src/config"],
-          "@shared": ["./src/shared"],
-          "@errors": ["./src/errors"],
-          "@utils": ["./src/utils"]
+          "@modules": "./src/modules",
+          "@config": "./src/config",
+          "@shared": "./src/shared",
+          "@errors": "./src/errors",
+          "@utils": "./src/utils",
         },
-      }
+      },
     ],
-    "@babel/plugin-transform-typescript",
+    "babel-plugin-transform-typescript-metadata",
     ["@babel/plugin-proposal-decorators", { legacy: true }],
     ["@babel/plugin-proposal-class-properties", { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
-
-  ]
-}
+  ],
+};
