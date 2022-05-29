6ee1ceeefda66e70a073a1bc6c50135d001de079
Tales <tales.monteiro02@hotmail.com>
2022-05-28 23:38:03 -0300

    correções

--- src/shared/infra/http/middlewares/raterLimiter.ts
+++ src/shared/infra/http/middlewares/raterLimiter.ts
@@ -4,30 +4,29 @@ import * as redis from "redis";
 
 import { AppError } from "@shared/errors/AppError";
 
-const redisClient = redis.createClient({
-  legacyMode: true,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-    sessionTimeout: 20,
-  },
-});
-
-const limiter = new RateLimiterRedis({
-  storeClient: redisClient,
-  keyPrefix: "rateLimiter",
-  points: 10,
-  duration: 5,
-});
-
 export default async function rateLimiter(
   request: Request,
   response: Response,
   next: NextFunction
 ): Promise<void> {
-  try {
-    await redisClient.connect();
+  const redisClient = redis.createClient({
+    legacyMode: true,
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+      sessionTimeout: 20,
+    },
+  });
+  await redisClient.connect();
 
+  const limiter = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: "rateLimiter",
+    points: 10,
+    duration: 5,
+  });
+
+  try {
     await limiter.consume(request.ip);
 
     return next();
