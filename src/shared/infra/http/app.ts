3856629e72418408feebc821a8bac8b637cc173d
Tales <tales.monteiro02@hotmail.com>
2022-05-28 23:16:57 -0300

    implemented Sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,6 +5,8 @@ import cors from "cors";
 
 import "dotenv/config";
 import "reflect-metadata";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import raterLimiter from "@shared/infra/http/middlewares/raterLimiter";
 
 import swaggerFile from "../../../swagger.json";
@@ -19,6 +21,19 @@ const app = express();
 
 app.use(cors());
 app.use(raterLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -26,6 +41,8 @@ app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
